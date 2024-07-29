45bd81138a0a249f8842be97e0421143b79cd117
PMFrancisco <francisco.perez.munoz@gmail.com>
2024-07-29 19:09:17 +0200

    fix: opaque background so the stars dont overlap

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -2,7 +2,7 @@ import { Text } from "../atoms/Text"
 
 export const Header = () => {
   return (
-    <header>
+    <header className="bg-black bg-opacity-50">
         <Text as="h1" variant="MSemiBold">Francisco Pérez</Text>
         <Text as="h2" variant="SSemiBold">Bienvenido a mi portfolio personal</Text>
     </header>
--- components/molecules/Main.tsx
+++ components/molecules/Main.tsx
@@ -2,7 +2,7 @@ import { Text } from "../atoms/Text"
 
 export const Main = () => {
   return (
-    <main>
+    <main className="bg-black bg-opacity-50">
         <Text as="p" variant="SRegular">
             Aquí encontrarás información sobre mis proyectos, experiencia y habilidades.
         </Text>
